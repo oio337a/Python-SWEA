002ed186697611c047394db9d9a348fc308b2d74
oio337a <oio337a@gmail.com>
2023-02-20 13:41:44 +0900

    2019 mission clear

---
+++ 2019.py
@@ -0,0 +1,10 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QDEX6AqwDFAUq&categoryId=AV5QDEX6AqwDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=30&pageIndex=1
+
+# 더블더블
+
+n = int(input())
+
+result = 1
+for i in range(n + 1):
+    print(result, end=" ")
+    result *= 2
