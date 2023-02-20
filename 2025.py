36ee032a93245564c86d38a137c5d761c1e764e9
oio337a <oio337a@gmail.com>
2023-02-20 13:28:31 +0900

    2025 mission clear

---
+++ 2025.py
@@ -0,0 +1,7 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QFZtaAscDFAUq&categoryId=AV5QFZtaAscDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=30&pageIndex=1
+
+# N줄덧셈
+
+n = int(input())
+
+print(sum([i for i in range(n + 1)]))
