a758f42987b29a612ab1f3751fc65208b7a44308
oio337a <oio337a@gmail.com>
2023-02-20 13:43:41 +0900

    1545 mission clear

---
+++ 1545.py
@@ -0,0 +1,8 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV2gbY0qAAQBBAS0&categoryId=AV2gbY0qAAQBBAS0&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=30&pageIndex=1
+
+# 거꾸로 출력해 보아요
+
+n = int(input())
+
+for i in range(n, -1, -1):
+    print(i, end=" ")
