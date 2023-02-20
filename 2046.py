6de386574f5690323b94ae952dde095e3c67eb20
oio337a <oio337a@gmail.com>
2023-02-20 13:17:53 +0900

    2046 mission clear

---
+++ 2046.py
@@ -0,0 +1,7 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QKdT6AyYDFAUq&categoryId=AV5QKdT6AyYDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=30&pageIndex=1
+
+# 스탬프 찍기
+
+n = int(input())
+
+print("#" * n)
