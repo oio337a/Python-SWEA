84e572b92c278609f1f45eae453f082eae722740
oio337a <oio337a@gmail.com>
2023-02-20 12:49:19 +0900

    2058 mission clear

---
+++ 2058.py
@@ -0,0 +1,11 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QPRjqA10DFAUq&categoryId=AV5QPRjqA10DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=30&pageIndex=1
+
+# 자릿수 더하기
+
+n = input()
+
+result = 0
+for i in n:
+    result += int(i)
+
+print(result)
