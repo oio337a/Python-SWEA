de71f4be3b5700f824bffef6390fb44caa140886
oio337a <oio337a@gmail.com>
2023-02-20 13:20:20 +0900

    2043 mission clear

---
+++ 2043.py
@@ -0,0 +1,7 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QJ_8KAx8DFAUq&categoryId=AV5QJ_8KAx8DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=30&pageIndex=1
+
+# 서랍의 비밀번호
+
+k, p = map(int, input().split())
+
+print(abs(k - p) + 1)
