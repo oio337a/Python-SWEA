403db9974caf89c467d9b031fb7ad15c30de7248
oio337a <oio337a@gmail.com>
2023-02-20 13:30:48 +0900

    1938 mission clear

---
+++ 1938.py
@@ -0,0 +1,7 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5PjsYKAMIDFAUq&categoryId=AV5PjsYKAMIDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=30&pageIndex=1
+
+# 아주 간단한 계산기
+
+a, b = map(int, input().split())
+
+print(a+b, a-b, a*b, a//b, sep="\n")
