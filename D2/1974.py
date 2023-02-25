91d5a2672606dafbe7cfb6ca0a62dbea98496f2d
oio337a <oio337a@gmail.com>
2023-02-25 17:22:32 +0900

    1974 mission clear

---
+++ D2/1974.py
@@ -0,0 +1,39 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5Psz16AYEDFAUq&categoryId=AV5Psz16AYEDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=2
+
+# 스도쿠 검증
+T = int(input())
+for tc in range(1, T+1):
+    arr = [list(map(int, input().split())) for _ in range(9)]
+
+    result = 1
+    # 가로 세로 검사
+    for i in range(9):
+        cnt_r = [0] * 10
+        cnt_c = [0] * 10
+        for j in range(9):
+            cnt_r[arr[i][j]] += 1
+            cnt_c[arr[j][i]] += 1
+
+        # 중복 체크
+        for k in range(1, 10):
+            if cnt_r[k] != 1:
+                result = 0
+                break
+            if cnt_c[k] != 1:
+                result = 0
+                break
+
+    # 3x3 격자 검사
+    for i in range(3):
+        for j in range(3):
+            cnt_x = [0] * 10
+            for k in range(3):
+                for l in range(3):
+                    cnt_x[arr[3*i+k][3*j+l]] += 1
+
+            for k in range(1, 10):
+                if cnt_x[k] != 1:
+                    result = 0
+                    break
+
+    print("#{} {}".format(tc, result))
